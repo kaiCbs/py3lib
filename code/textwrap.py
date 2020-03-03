840664de264d6c4bba2f7086b17131dfc0576f95
Kai <kxiang21@gsb.columbia.edu>
2020-03-02 19:41:50 -0500

    finish textwrap

--- code/textwrap.py
+++ code/textwrap.py
@@ -1,8 +1,41 @@
+import textwrap
+
 # textwrap: Formatting Text Paragraphs
 # The textwrap module can be used to format text for output in situations where prettyprinting
 # is desired. It offers programmatic functionality similar to the paragraph wrapping
 # or filling features found in many text editors and word processors.
 
 sample_text = """
-
+    The textwrap module can be used to format text for output in situations where prettyprinting
+    is desired. It offers programmatic functionality similar to the paragraph wrapping or filling
+    features found in many text editors and word processors.
 """
+
+
+# ----------------------------------
+# Filling Paragraphs
+
+print(sample_text)
+print(textwrap.fill(sample_text, width=50))
+
+
+# ----------------------------------
+# Removing Existing Indentation
+# Removing the common whitespace prefix from all of the lines in the sample text with dedent() 
+# produces better results and allows the use of docstrings or embedded multiline strings straight 
+# from Python code while removing the formatting of the code itself. 
+
+denteded = textwrap.dedent(sample_text)
+print(denteded)
+
+
+# ----------------------------------
+# Indenting Blocks
+
+print(textwrap.indent(denteded, ">> "))
+
+
+# ----------------------------------
+# Truncating Long Text
+
+print(textwrap.shorten(sample_text, 100))
