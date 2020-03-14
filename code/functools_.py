95a0ce450d500bfbc462fa8bcb1a41de9b004f60
Kai <kxiang21@gsb.columbia.edu>
2020-03-14 19:59:13 -0400

    add functools

---
+++ code/functools_.py
@@ -0,0 +1,8 @@
+# functools: Tools for Manipulating Functions
+# The functools module provides tools for adapting or extending functions and other callable objects, without completely rewriting them
+
+import functools
+
+
+# ----------------------------------
+# Decorator
