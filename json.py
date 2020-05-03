9edcb7d99234a8756f0b4e42d1437e016c10c7d5
kai <kxiang21@gsb.columbia.edu>
2020-05-03 01:13:34 -0400

    add

---
+++ json.py
@@ -0,0 +1,4 @@
+# json: JavaScript Object Notation
+# The json module provides an API similar to pickle (page 396) for converting
+# in-memory Python objects to a serialized representation known as JavaScript
+# Object Notation (JSON).
